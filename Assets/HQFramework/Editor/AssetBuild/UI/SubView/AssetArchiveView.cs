46a10f763071397a91a8b85731f82d981610b780
haonan <haonan_happyq@163.com>
2024-09-26 00:59:34 +0800

    optimize asset archive gui

--- Assets/HQFramework/Editor/AssetBuild/UI/SubView/AssetArchiveView.cs
+++ Assets/HQFramework/Editor/AssetBuild/UI/SubView/AssetArchiveView.cs
@@ -10,6 +10,8 @@ namespace HQFramework.Editor
         private Vector2[] moduleScrollPosList;
         private Vector2 scrollPos;
         private Dictionary<int, int> selectedModuleDic;
+        private GUIStyle moduleTitleStyle;
+        private GUIStyle middleCenterVerticalStyle;
 
         public AssetArchiveView(EditorWindow baseWindow, GUIContent tabTitle) : base(baseWindow, tabTitle)
         {
@@ -17,6 +19,15 @@ namespace HQFramework.Editor
 
         public override async void OnEnable()
         {
+            GUIStyleState normalState = new GUIStyleState();
+            normalState.background = Texture2D.blackTexture;
+            normalState.textColor = new Color(1, 1, 1, 0.6f);
+            moduleTitleStyle = new GUIStyle();
+            moduleTitleStyle.fontSize = 15;
+            moduleTitleStyle.alignment = TextAnchor.MiddleLeft;
+            moduleTitleStyle.contentOffset = new Vector2(10, 25);
+            moduleTitleStyle.normal = normalState;
+
             moduleList = new List<List<AssetModuleBuildResult>>();
             selectedModuleDic = new Dictionary<int, int>();
             AssetModuleBuildHistoryData historyData = await AssetArchiver.LoadBuildHistoryData();
@@ -36,6 +47,7 @@ namespace HQFramework.Editor
 
         public override void OnGUI()
         {
+            GUILayout.BeginArea(new Rect(10, 10, viewRect.width - 20, viewRect.height - 10));
             GUILayout.BeginVertical();
             scrollPos = GUILayout.BeginScrollView(scrollPos);
             scrollPos.x = 0;
@@ -45,39 +57,45 @@ namespace HQFramework.Editor
                 GUILayout.Space(5);
             }
             GUILayout.EndScrollView();
+            GUILayout.EndVertical();
 
-            GUILayout.Space(10);
+
+            GUILayout.FlexibleSpace();
 
             if (GUILayout.Button("Archive", GUILayout.Height(45)))
             {
 
             }
-
-            GUILayout.EndVertical();
+            GUILayout.Space(10);
+            GUILayout.EndArea();
         }
 
         private void DrawMoudleHistory(int index, List<AssetModuleBuildResult> moduleBuildResultList)
         {
-            GUILayout.BeginHorizontal("PopupCurveSwatchBackground", GUILayout.Height(60));
+            GUIStyle backgroundStyle = "PopupCurveSwatchBackground";
+            GUILayout.BeginHorizontal(backgroundStyle, GUILayout.Height(80));
 
             GUIContent moduleContent = EditorGUIUtility.IconContent("d_UnityLogo");
-            moduleContent.text = moduleBuildResultList[0].moduleName;
-            GUILayout.Box(moduleContent);
+            moduleContent.text = $" {moduleBuildResultList[0].moduleName}";
+            GUILayout.Box(moduleContent, moduleTitleStyle, GUILayout.Width(90), GUILayout.Height(30));
 
             GUILayout.Space(10);
 
-            moduleScrollPosList[index] = GUILayout.BeginScrollView(moduleScrollPosList[index]);
+            GUILayout.BeginVertical();
+            GUILayout.Space(7.5f);
+            moduleScrollPosList[index] = GUILayout.BeginScrollView(moduleScrollPosList[index], GUIStyle.none, GUIStyle.none);
             moduleScrollPosList[index].y = 0;
             GUILayout.BeginHorizontal();
+            GUILayout.Space(10);
             for (int i = 0; i < moduleBuildResultList.Count; i++)
             {
                 AssetModuleBuildResult moduleResult = moduleBuildResultList[i];
                 GUIStyle btnStyle = "AppToolbarButtonRight";
                 if (selectedModuleDic.ContainsKey(index) && selectedModuleDic[index] == i)
                 {
-                    btnStyle = "AppToolbarButtonMid";
+                    btnStyle = "ButtonRight";
                 }
-                if (GUILayout.Button(moduleResult.buildVersionCode.ToString(), btnStyle, GUILayout.Width(100), GUILayout.Height(60)))
+                if (GUILayout.Button($"Build Code: {moduleResult.buildVersionCode}", btnStyle, GUILayout.Width(100), GUILayout.Height(60)))
                 {
                     if (selectedModuleDic.ContainsKey(index))
                     {
@@ -94,12 +112,15 @@ namespace HQFramework.Editor
                     {
                         selectedModuleDic.Add(index, i);
                     }
-                    GUILayout.Space(5);
                 }
+                GUILayout.Space(5);
             }
             GUILayout.EndHorizontal();
             GUILayout.EndScrollView();
+            GUILayout.EndVertical();
+            GUILayout.Space(5);
             GUILayout.EndHorizontal();
+            GUILayout.Space(5);
         }
 
         public override void OnDisable()
