67fad88df7b25d3bede31e0ce709fccac15c4fc2
haonan <haonan_happyq@163.com>
2024-10-09 10:47:06 +0800

    optimize asset config serializer

--- Assets/HQFramework/Editor/AssetBuild/Utility/AssetUtility.ConfigSerializer.cs
+++ Assets/HQFramework/Editor/AssetBuild/Utility/AssetUtility.ConfigSerializer.cs
@@ -1,4 +1,7 @@
+using System;
+using System.IO;
 using System.Text;
+using UnityEngine;
 
 namespace HQFramework.Editor
 {
@@ -16,6 +19,38 @@ namespace HQFramework.Editor
             {
                 string jsonStr = Encoding.UTF8.GetString(data);
                 return JsonUtilityEditor.ToObject<T>(jsonStr);
+            }
+
+            public static bool SerializeToFile(object target, string path)
+            {
+                string jsonStr = JsonUtilityEditor.ToJson(target);
+                string dir = Path.GetDirectoryName(path);
+                if (!Directory.Exists(dir))
+                {
+                    Directory.CreateDirectory(dir);
+                }
+                try
+                {
+                    File.WriteAllText(path, jsonStr);
+                    return true;
+                }
+                catch (Exception ex)
+                {
+                    Debug.Log(ex.Message);
+                    return false;
+                }
+            }
+
+            public static T DeserializeFromFile<T>(string path) where T : class
+            {
+                if (!File.Exists(path))
+                {
+                    return null;
+                }
+
+                string jsonStr = File.ReadAllText(path);
+                T result = JsonUtilityEditor.ToObject<T>(jsonStr);
+                return result;
             }
         }
     }
